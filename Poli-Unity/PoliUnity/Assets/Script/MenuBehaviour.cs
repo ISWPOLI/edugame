0565d9d6f1489da2d2f696ef8a381f63bf9ceff9
alejandro995 <alejodt995@gmail.com>
2017-11-04 19:11:42 -0500

    Diseño Inicial
    Se agrega el diseño del escenario, de el reverso de la cartas.  (Un
    diseño basico para probar scripts y animacion).

---
+++ Poli-Unity/PoliUnity/Assets/Script/MenuBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour {
+
+	public void triggerMenuBehavior(int i){
+        switch(i){
+        default:
+        case(1):
+            SceneManager.LoadScene("PlayScene");
+        break;
+        case(2):
+             SceneManager.LoadScene("Puntuaciones");
+        break;
+        case(0):
+            Application.Quit ();
+        break;
+
+        }
+
+	}
+}
